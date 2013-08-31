b566032991350ce2e37805cc5e716ded9902cd63
Elmoz <amro.elmo@gmail.com>
2013-08-31 15:12:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Azbakeya1::Application.config.secret_key_base = 'e87f688bfed785717d7ad79569902470200958d5a9d2c56825cd32948d8144ca1dec47e68113d38b60ff5ebb3cd96bb8d88c7a6196da30db0529cd5b29665ad4'
